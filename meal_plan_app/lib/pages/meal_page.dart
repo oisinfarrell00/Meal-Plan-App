5a87be224dd9c71559f4c35e4cc25e1201b18980
oisin farrell <oisin.farrell2000@gmail.com>
2023-08-24 17:31:00 +0100

    meal page ui cleaned up

--- meal_plan_app/lib/pages/meal_page.dart
+++ meal_plan_app/lib/pages/meal_page.dart
@@ -16,85 +16,93 @@ class _MealPageState extends State<MealPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Expanded(
-            flex: 1,
-            child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  const Text(
-                    "My Meals",
-                    style: constants.MEAL_HEADER_TEXT_STYLE,
-                  ),
-                  SizedBox(
-                    width: constants.CONTENT_WIDTH,
-                    child: ElevatedButton(
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const CreateMealPage()),
-                          );
-                        },
-                        child: const Text("Add Meal")),
-                  )
-                ],
-              ),
-            )),
-        Expanded(
-          flex: 3,
-          child: StreamBuilder(
-              stream:
-                  FirebaseFirestore.instance.collection('Meals').snapshots(),
-              builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
-                if (!snapshot.hasData) {
-                  return const CircularProgressIndicator();
-                } else {
-                  final meals = snapshot.data.docs;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("My Meals"),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ElevatedButton.icon(
+              style: ButtonStyle(
+                  foregroundColor:
+                      MaterialStateProperty.all<Color>(Colors.white),
+                  backgroundColor: MaterialStateProperty.all<Color>(
+                      const Color.fromARGB(99, 64, 95, 220)),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(18.0),
+                          side: const BorderSide(color: Colors.white)))),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const CreateMealPage()),
+                );
+              },
+              icon: const Icon(Icons.add),
+              label: const Text("Create Meal"),
+            ),
+          )
+        ],
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            flex: 3,
+            child: StreamBuilder(
+                stream:
+                    FirebaseFirestore.instance.collection('Meals').snapshots(),
+                builder:
+                    (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+                  if (!snapshot.hasData) {
+                    return const CircularProgressIndicator();
+                  } else {
+                    final meals = snapshot.data.docs;
 
-                  var mealList = [];
-                  meals.forEach((meal) {
-                    mealList.add(meal.data());
-                  });
-                  return ListView.builder(
-                      itemCount: mealList.length,
-                      itemBuilder: ((context, index) {
-                        return ListTile(
-                          leading: const Icon(Icons.list),
-                          title: Text("${mealList[index]['name']}"),
-                          trailing: Row(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              IconButton(
-                                icon: const Icon(
-                                  Icons.remove_circle_outline,
-                                  color: Colors.redAccent,
-                                ),
-                                onPressed: () {
-                                  Meal mealToRemove = Meal(
-                                      name: mealList[index]['name'],
-                                      ingredients: ingredients);
-                                  mealToRemove
-                                      .removeMealFromDatabase(mealToRemove);
-                                },
+                    var mealList = [];
+                    meals.forEach((meal) {
+                      mealList.add(meal.data());
+                    });
+                    return ListView.builder(
+                        itemCount: mealList.length,
+                        itemBuilder: ((context, index) {
+                          return Card(
+                            child: ListTile(
+                              leading: const Icon(Icons.list),
+                              title: Text("${mealList[index]['name']}"),
+                              trailing: Row(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  IconButton(
+                                    icon: const Icon(
+                                      Icons.remove_circle_outline,
+                                      color: Color.fromRGBO(220, 50, 50, 150),
+                                    ),
+                                    onPressed: () {
+                                      Meal mealToRemove = Meal(
+                                          name: mealList[index]['name'],
+                                          ingredients: ingredients);
+                                      mealToRemove
+                                          .removeMealFromDatabase(mealToRemove);
+                                    },
+                                  ),
+                                  IconButton(
+                                    icon: const Icon(
+                                      Icons.edit,
+                                      color: Color.fromRGBO(50, 220, 50, 150),
+                                    ),
+                                    onPressed: () {},
+                                  ),
+                                ],
                               ),
-                              IconButton(
-                                icon: const Icon(
-                                  Icons.edit,
-                                  color: Colors.greenAccent,
-                                ),
-                                onPressed: () {},
-                              ),
-                            ],
-                          ),
-                        );
-                      }));
-                }
-              }),
-        ),
-      ],
+                            ),
+                          );
+                        }));
+                  }
+                }),
+          ),
+        ],
+      ),
     );
   }
 }
